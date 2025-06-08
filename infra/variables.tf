14356c238d4662d50f0956ac7948fb353d8a3d74
manishareddyj97 <manishareddyj97@gmail.com>
2025-06-08 15:16:59 -0400

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,10 @@
-fgjk,
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "cloud-retail-analytics"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
