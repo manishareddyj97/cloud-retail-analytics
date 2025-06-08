04d9aac854c13b245035a709a4758703cf230967
manishareddyj97 <manishareddyj97@gmail.com>
2025-06-08 15:15:37 -0400

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,1 +1,15 @@
-abc
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.4.0"
+}
